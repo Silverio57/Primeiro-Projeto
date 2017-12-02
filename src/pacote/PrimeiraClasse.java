d0aaae2c0e1059caa7d2f7c50c1bdf076ebb2b36
Silverio57 <34194242+Silverio57@users.noreply.github.com>
2017-12-02 19:01:00 -0200

    new commit

---
+++ src/pacote/PrimeiraClasse.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pacote;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Silvério Rodrigues
+ */
+public class PrimeiraClasse {
+    public static void main(String[] args) {
+        Scanner t = new Scanner(System.in);
+        
+        System.out.println("digite seu nome: ");
+        String a = t.next();
+        
+        System.out.println("Bem vindo "+a);
+    }
+}
